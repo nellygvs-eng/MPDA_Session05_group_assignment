12876ca77e101d0446f2c97a1c3936d461283a92
nellygvs-eng <nellygvs@gmail.com>
2026-05-14 20:32:10 +0200

    pseudo code

--- testnelly.py
+++
@@ -1,7 +0,0 @@
-CO = int(input('What do you have left in pesos?'))
-PE = int(input('What do you have left in soles?'))
-BR = int(input('What do you have left in reais?'))
-
-USD = (CO*0.0027) + (PE*0.29) + (BR*0.19)
-
-print(USD)
